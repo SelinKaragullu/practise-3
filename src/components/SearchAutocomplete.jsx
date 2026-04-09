8992f42e6c42e96c703ec457173747d0a4e5f410
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-09 16:04:40 +0300

    Update SearchAutocomplete.jsx

--- src/components/SearchAutocomplete.jsx
+++ src/components/SearchAutocomplete.jsx
@@ -1,6 +1,34 @@
 
-import {useState,useEffect}
+import { useState, useEffect } from 'react'
 
 export default function SearchAutocomplete() {
 
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    async function fetchListOfUsers() {
+
+        try {
+            setLoading(true)
+            const res = await fetch("https://dummyjson.com/users")
+            const data = await res.json()
+          if(data && data.users)  {setUsers(
+                data.users.map((user)=>user.firstName)
+            )}
+            setLoading(false)
+        } catch {
+            setLoading(false)
+        }
+    }
+
+useEffect (()=>{
+    fetchListOfUsers()
+},[])
+ 
+console.log(users)
+
+return (
+    loading? <div>Loading...</div> : <div>data is in console, check it out</div>
+)
 }
