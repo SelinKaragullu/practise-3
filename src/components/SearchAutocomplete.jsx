a2cffe0d10347a34191a8fe8fe2e37ddad75c14b
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-10 22:16:01 +0300

    Update SearchAutocomplete.jsx

--- src/components/SearchAutocomplete.jsx
+++ src/components/SearchAutocomplete.jsx
@@ -6,22 +6,23 @@ export default function SearchAutocomplete() {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
-    const [searchParam,setSearchParam] = useState("")
-    const [filteredUsers,setFilteredUsers] = useState([])
-    const [showDropdown,setShowDropdown] = useState(false)
+    const [searchParam, setSearchParam] = useState("")
+    const [filteredUsers, setFilteredUsers] = useState([])
+    const [showDropdown, setShowDropdown] = useState(false)
 
 
 
     function handleChange(event) {
-const query = event.target.value.toLowerCase()
-setSearchParam(query)
+        const query = event.target.value.toLowerCase()
+        setSearchParam(query)
 
-if(query.length>1) {const filteredUsersArray = users.filter((user)=>user.toLowerCase().includes(query))
-   setFilteredUsers(filteredUsersArray)
-setShowDropdown(true)
-}else{
-    setShowDropdown(false)
-}
+        if (query.length > 1) {
+            const filteredUsersArray = users.filter((user) => user.toLowerCase().includes(query))
+            setFilteredUsers(filteredUsersArray)
+            setShowDropdown(true)
+        } else {
+            setShowDropdown(false)
+        }
     }
 
     async function fetchListOfUsers() {
@@ -30,29 +31,32 @@ setShowDropdown(true)
             setLoading(true)
             const res = await fetch("https://dummyjson.com/users")
             const data = await res.json()
-          if(data && data.users)  {setUsers(
-                data.users.map((user)=>user.firstName)
-            )}
+            if (data && data.users) {
+                setUsers(
+                    data.users.map((user) => user.firstName)
+                )
+            }
             setLoading(false)
-        } catch(err) {
+        } catch (err) {
             setLoading(false)
             setError(err.message)
             console.log(err)
         }
     }
 
-useEffect (()=>{
-    fetchListOfUsers()
-},[])
- 
-console.log(users)
+    useEffect(() => {
+        fetchListOfUsers()
+    }, [])
 
-return (
-    loading? <div>Loading...</div> : 
-   <input 
-   type="text" 
-   onChange={handleChange}
-   placeholder="Type user..."
-   />
-)
+    console.log(users)
+
+    return (
+        loading ? <div>Loading...</div> :
+            <input
+                type="text"
+                value={searchParam}
+                onChange={handleChange}
+                placeholder="Type user..."
+            />
+    )
 }
