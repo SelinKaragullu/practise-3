893ed3adfba2c0ee2acab8bd76b9de7c6cddf1a8
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-12 13:25:27 +0300

    Update ScrollIndicator.jsx

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -7,6 +7,7 @@ export default function ScrollIndicator ({url}) {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState("")
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
 
   async function fetchData (url) {
@@ -28,14 +29,36 @@ useEffect(()=>{
 },[url])
 
 
+useEffect(()=>{
+window.addEventListener("scroll",handleScrollPercentage)
 
-return (<>
-    {loading && <div>Loading...</div>} 
-{data && data.length>0 && data.map((dataItem)=><p key={dataItem.id}>{dataItem.title}</p>
-)
+return ()=>{
+  window.removeEventListener("scroll",handleScrollPercentage)
+}
+},[])
 
+function handleScrollPercentage() {
+    const howMuchScrolled = document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    setScrollPercentage((howMuchScrolled / height) * 100);
 }
 
+
+return (<>
+<div className="top-container">
+       <div className="scroll-progress-tracking-container">
+        <div
+          className="current-progress-bar"
+          style={{ width: `${scrollPercentage}%` }}
+        ></div>
+      </div>
+    </div>
+
+<div>
+    {loading && <div>Loading...</div>} 
+{data && data.length>0 && data.map((dataItem)=><p key={dataItem.id}>{dataItem.title}</p>
+)}
+</div>
 </>
 )
 }
