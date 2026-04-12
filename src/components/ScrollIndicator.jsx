125e8bc556e9ca5368715e7ce6dbddd4e4dba5c8
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-12 12:29:55 +0300

    Update ScrollIndicator.jsx

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -7,4 +7,35 @@ export default function ScrollIndicator ({url}) {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [errorMessage, setErrorMessage] = useState("")
+
+
+  async function fetchData (url) {
+    try{
+    setLoading(true)
+   const res = await fetch(url)
+   const data = await res.json() 
+    setData(data.products)
+    setLoading(false)
+    } catch (err) {
+        setLoading(false)
+        setErrorMessage(err)
+    }
+  }
+
+
+useEffect(()=>{
+    fetchData(url)
+},[url])
+
+
+
+return (<>
+    {loading && <div>Loading...</div>} 
+{data && data.length>0 && data.map((dataItem)=><p key={dataItem.id}>{dataItem.title}</p>
+)
+
+}
+
+</>
+)
 }
