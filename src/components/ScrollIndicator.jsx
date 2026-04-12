04d9628e663af1f8991f21b3e72d5f214a080b6d
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-12 11:03:37 +0300

    Create ScrollIndicator.jsx

---
+++ src/components/ScrollIndicator.jsx
@@ -0,0 +1,10 @@
+import {useState,useEffect} from 'react'
+
+
+
+export default function ScrollIndicator ({url}) {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+}
