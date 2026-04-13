e0ada32af405135dc279e8fbd75da77c36c34fa6
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-13 21:26:33 +0300

    new game setup

---
+++ src/components/Square.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Square ({value,onClick}) {
+    return <button></button>
+}
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,12 @@
+
+
+export default function TicTacToe () {
+    const [board,setBoard] = useState(Array(9).fill(""))
+    const [turn,setTurn] = useState("X")
+  
+
+    return <div>
+
+        
+    </div>
+}
