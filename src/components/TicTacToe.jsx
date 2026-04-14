b7e7e043ed518b5dc0102dc44cce7e0139922f32
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-14 09:11:40 +0300

    Update TicTacToe.jsx

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -7,9 +7,31 @@ export default function TicTacToe () {
     const [turn,setTurn] = useState("X")
   
 
+    function handleClick (index) {
+const cpyBoard = [...board]
+if(cpyBoard[index] !== "") return 
+cpyBoard[index] = turn
+setBoard(cpyBoard)
+setTurn(turn==="X" ? "O" : "X")
+    }
+
+
+    function getWinner(squares) {
+    const winningPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Yatay
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Dikey
+        [0, 4, 8], [2, 4, 6]             // Çapraz
+    ]
+
+    // BURAYI SEN DOLDUR: 
+    // Bir for döngüsü ile bu winningPatterns dizisini dön.
+    // Eğer squares[a], squares[b] ve squares[c] birbirine eşitse ve boş değilse, kazananı (squares[a]) döndür (return et).
+    // Döngü biterse ve kimse kazanmamışsa 'null' döndür.
+}
+
     return <div>
-<div><Square onClick={()=>{}} value={board[0]} /><Square onClick={()=>{}} value={board[1]}/><Square onClick={()=>{}} value={board[2]} /></div>
-<div><Square onClick={()=>{}} value={board[3]}/><Square onClick={()=>{}} value={board[4]} /><Square onClick={()=>{}} value={board[5]} /></div>
-<div><Square onClick={()=>{}} value={board[6]} /><Square onClick={()=>{}} value={board[7]} /><Square onClick={()=>{}} value={board[8]} /></div>
+<div><Square onClick={()=>{handleClick(0)}} value={board[0]} /><Square onClick={()=>{handleClick(1)}} value={board[1]}/><Square onClick={()=>{handleClick(2)}} value={board[2]} /></div>
+<div><Square onClick={()=>{handleClick(3)}} value={board[3]}/><Square onClick={()=>{handleClick(4)}} value={board[4]} /><Square onClick={()=>{handleClick(5)}} value={board[5]} /></div>
+<div><Square onClick={()=>{handleClick(6)}} value={board[6]} /><Square onClick={()=>{handleClick(7)}} value={board[7]} /><Square onClick={()=>{handleClick(8)}} value={board[8]} /></div>
     </div>
 }
