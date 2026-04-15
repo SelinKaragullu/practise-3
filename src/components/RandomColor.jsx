92ae670a90d624e411c11492eef8b72a122551e6
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-15 16:26:26 +0300

    Update RandomColor.jsx

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -1,3 +1,17 @@
+import {useState} from 'react'
+
+
 export default function RandomColor () {
-    return <div></div>
+const[typeOfColor,setTypeOfColor] = useState("hex")
+const[color,setColor] = useState("#000000")
+let style = {width:"100vw", height:"100vh", backgroundColor:color}
+
+    return <div style={style}>
+    
+    <button onClick={()=>{}}>select HEX color</button>
+    <button onClick={()=>{}}>select RGB color</button>
+    <button onClick={()=>{}}>select Random color</button>
+    <h1>{color}</h1>
+       
+    </div>
 }
