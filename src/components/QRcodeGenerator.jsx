64cd65f75db1ee60a1daa948295cf990d3bd4ac2
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-16 10:57:09 +0300

    Update QRcodeGenerator.jsx

--- src/components/QRcodeGenerator.jsx
+++ src/components/QRcodeGenerator.jsx
@@ -1,24 +1,39 @@
-import {useState} from 'react'
+import { useState } from 'react'
+import QRCode from 'react-qr-code'
 
 
-export default function QRcodeGenerator () {
-const[query,setQuery] = useState ("")
+export default function QRcodeGenerator() {
+    const [query, setQuery] = useState("")
+    const [qrCode, setQrCode] = useState("")
 
-function handleQuery (e) {
-   setQuery (e.target.value)
-   console.log(e.target.value)
-}
+    function handleQuery(e) {
+        setQrCode("")
+        setQuery(e.target.value)
+        console.log(e.target.value)
+    }
 
-function handleGenerate () {
-     console.log(query)
-    setQuery("")
-}
+    function handleGenerate() {
+        console.log(query)
+        setQrCode(query)
+        setQuery("")
+    }
 
+
+    function reset() {
+        setQrCode("")
+        setQuery("")
+    }
     return <>
-    <input 
-    onChange={handleQuery}
-    value={query} 
-    type="text" />
-    <button onClick={handleGenerate}>Generate</button>
+
+        <input
+            onChange={handleQuery}
+            value={query}
+            type="text" />
+        <button disabled={query.trim() === ""} onClick={handleGenerate}>Generate</button>
+        {qrCode && <QRCode value={qrCode} />}
+
+        <button onClick={reset}>Reset</button>
+
+
     </>
 }
