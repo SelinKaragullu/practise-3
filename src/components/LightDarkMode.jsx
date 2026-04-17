9c28203e833a4fac067e01d44518485080d3d02f
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-17 19:57:42 +0300

    Create LightDarkMode.jsx

---
+++ src/components/LightDarkMode.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+
+export default function LightDarkMode () {
+const [theme,setTheme] = useState('light')
+
+function handleToggleTheme () {
+    setTheme(theme==='dark' ? 'light':'dark')
+}
+
+
+return <div>
+    <p>{theme}</p>
+    <button onClick={handleToggleTheme}></button>
+
+</div>
+}
