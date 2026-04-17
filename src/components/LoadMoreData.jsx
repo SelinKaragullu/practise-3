b47d6b91a2815f1f303e8419821b202d74f2950f
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-17 12:24:52 +0300

    Update LoadMoreData.jsx

--- src/components/LoadMoreData.jsx
+++ src/components/LoadMoreData.jsx
@@ -4,12 +4,14 @@ export default function LoadMoreData () {
 
 const[products,setProducts] = useState([])
 const [count,setCount] = useState(0)
-
+const [loading,setLoading] = useState(false)
 
 async function fetchProducts() {
+    setLoading(true)
     const res = await fetch(`https://dummyjson.com/products?limit=20&skip=${count*20}`)
     const data = await res.json()
-    setProducts(data.products)
+    setProducts((prevProducts=>[...prevProducts, ...data.products]))
+     setLoading(false)
 }
 
 useEffect(()=>{
@@ -17,7 +19,7 @@ fetchProducts()
 },[count])
 
 return <>
-<div>Loading...</div>
+{loading? <div>Loading...</div> : null}
 {products.map((item)=><div key={item.id}>
 <div>{item.title}</div>
 <img src={item.thumbnail} alt="" />
