3fedb15d26aa33328b4ef60618fa285cb55b9388
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-19 20:18:29 +0300

    Update ImageSlider.jsx

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import {useState,useEffect} from 'react'
 
 export default function ImageSlider() {
 
     const [currentSlide, setCurrentSlide] = useState(0)
+    const [images,setImages] = useState([])
+    const [loading,setLoading] = useState(false)
+
+
+function handleNext () {
+ currentSlide===4 ? setCurrentSlide(0) : setCurrentSlide(prev=>prev+1)
+}
+
+function handlePrevious () {
+    currentSlide===0 ? setCurrentSlide(4) : setCurrentSlide(prev=>prev-1)
+}
+
+async function fetchImages() {
+    setLoading(true)
+    const res = await fetch ('https://picsum.photos/v2/list?page=1&limit=5')
+    const data = await res.json()
+    setImages(data)
+    setLoading(false)
+}
+
+useEffect(()=>{
+    fetchImages()
+},[])
 
     return <><button onClick={handlePrevious}> Previous </button>
-<h1>{currentSlide}</h1>
+
+{images.map((item,index)=>(
+    <div key={item.id}>
+<img className ={currentSlide===index? "active" : "hidden"}  src={item.download_url} alt="" />
+
+    </div>
+))}
+
+
     <button onClick={handleNext}> Next </button>
+
+
+
     </>
 }
