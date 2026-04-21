cec720382932ef1bb5c2baf865ebf47f286205c3
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-21 19:58:03 +0300

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,6 +7,7 @@ const createdDate = new Date(user.created_at)
     return (<>
         <img src={user.avatar_url} alt="Profile" />
         <h3>{user.name || user.login}</h3>
+       <h4> User joined on {createdDate.getFullYear()}</h4>
        <div>{user.public_repos}
 
 {user.followers}
