905e39c9904dc7e6a9c0461ee225bcf07d124f96
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-21 19:58:32 +0300

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,18 +1,17 @@
 
 
 
-export default function User ({user}) {
-const createdDate = new Date(user.created_at)
+export default function User({ user }) {
+    const createdDate = new Date(user.created_at)
 
     return (<>
         <img src={user.avatar_url} alt="Profile" />
         <h3>{user.name || user.login}</h3>
-       <h4> User joined on {createdDate.getFullYear()}</h4>
-       <div>{user.public_repos}
-
-{user.followers}
-
-{user.following}</div>
-        </>
+        <h4> User joined on {createdDate.getFullYear()}</h4>
+        <div>{user.public_repos}
+            {user.followers}
+            {user.following}
+        </div>
+    </>
     )
 }
