bcbe8eb72a121a8f096fb35f715e6e79f15754e9
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-21 12:48:40 +0300

    Update GithubProfileFinder.jsx

--- src/components/GithubProfileFinder.jsx
+++ src/components/GithubProfileFinder.jsx
@@ -3,11 +3,24 @@ import { useState } from "react";
 
 export default function GithubProfileFinder () {
     const [userName,setUserName] = useState("")
+    const [userData,setUserData] = useState(null)
+    const [loading,setLoading] = useState(false)
 
     function handleSubmit() {
-console.log(userName)
+fetchGithubUserData()
     }
 
+    async function fetchGithubUserData () {
+        setLoading(true)
+            const res = await fetch(`https://api.github.com/users/${userName}`)
+            const data = await res.json()
+            setUserData(data)
+            setLoading(false)
+            setUserName("")
+    }
+
+
+    console.log(userData)
     return <>
     <input type="text" value={userName} onChange={(event)=>setUserName(event.target.value)}/>
     <button onClick={handleSubmit}>Search</button>
