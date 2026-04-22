bd05a8bb2654f47b25b3f9f440cb70b20d0b2605
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-22 07:28:04 +0300

    updating context

--- src/components/data.js
+++ src/components/data.js
@@ -5,11 +5,13 @@ showLightAndDarkMode:true,
 showTicTacToeBoard: false
 }
 
+
+
 export default function featureFlagsDataServiceCall (){
      return new Promise((resolve, reject) => {
-    if (dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
-    else reject("Some error occured! Please try again");
-  });
+    if (dummyApiResponse) setTimeout(()=>resolve(dummyApiResponse), 500)
+    else reject("Some error occured! Please try again")
+  })
 }
 
 
