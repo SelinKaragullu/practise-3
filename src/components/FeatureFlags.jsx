4cd82779a4dc58dc0ba78b47bc45159c1a038f37
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-23 09:26:58 +0300

    Update FeatureFlags.jsx

--- src/components/FeatureFlags.jsx
+++ src/components/FeatureFlags.jsx
@@ -7,14 +7,15 @@ export default function FeatureFlags () {
 const {loading, enabledFlags} = useContext(FeatureFlagsContext)
 
 const componentsToRender = [
-  { key: "showLightAndDarkMode", component: <div>Light/Dark Mode Componenti</div> },
+  { key: "showLightAndDarkMode", component: <div >Light/Dark Mode Componenti</div> },
   { key: "showTicTacToeBoard", component: <div>Tic Tac Toe Componenti</div> }
 ]
 
 
-return <>{loading? <div>Tic Tac Toe Componenti</div> : null}
-    {componentsToRender.map((component)=>enabledFlags[component.key]? enabledFlags[component] : null
-
+return <>{loading? <div>Loading...</div> : null}
+    {componentsToRender.map((component)=> <div key={component.key}>{
+    enabledFlags[component.key]? enabledFlags[component.component] : null}
+</div>
     )}
 </>
 }
