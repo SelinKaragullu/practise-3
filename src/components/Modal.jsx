b99e63bea53d0119a825794b6898f1065e223a66
Selin Karagullu <selin.karagullu@gmail.com>
2026-04-27 14:01:43 +0300

    Create Modal.jsx

---
+++ src/components/Modal.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Modal({ id, header, body, footer, onClose }) {
+
+    return <div id={id || "Modal"} className="modal">
+
+        <div className="modal-content">
+            <div className="header">
+                <span onClick={onClose} className="close-modal-icon">&times;</span>
+                {header ? header : <h2>Header</h2>}
+            </div>
+            {body ? body : <h2>this is our Modal body</h2>}
+            <div className="footer">
+                <h2>Footer</h2>
+            </div>
+        </div>
+    </div>
+
+}
